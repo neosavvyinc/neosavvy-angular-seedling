ba4be32ff828cb72cc068c73600ab79fec26a8a3
Adam Parrish <aparrish@neosavvy.com>
2013-09-27 16:00:21 -0400

    Adding in a landing page with zurb foundation.

--- src/main/resources/application/application.js
+++ src/main/resources/application/application.js
@@ -7,9 +7,35 @@ Neosavvy.Filters = angular.module('neosavvy.filters', []);
 Neosavvy.Directives = angular.module('neosavvy.directives', []);
 
 
-angular.module('application', ['neosavvy.filters', 'neosavvy.services', 'neosavvy.directives', 'neosavvy.constants', 'neosavvy.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    
-    $routeProvider.
-      otherwise({templateUrl: 'application/view/content/content.html'});
-  }]);
+angular.module(
+    'application',
+    [
+        'neosavvy.filters',
+        'neosavvy.services',
+        'neosavvy.directives',
+        'neosavvy.constants',
+        'neosavvy.controllers'
+    ]
+).
+  config(
+    [
+        '$routeProvider',
+        function($routeProvider) {
+
+
+        $routeProvider.when('/index', {
+            templateUrl: 'view/content/home-ptl.html',
+            controller: HomeController,
+            controllerAs: 'book'
+        });
+        $routeProvider.when('/Book/:bookId/ch/:chapterId', {
+            templateUrl: 'chapter.html',
+            controller: ChapterCntl,
+            controllerAs: 'chapter'
+        });
+
+        $routeProvider.
+            otherwise({templateUrl: 'application/view/content/content.html'});
+        }
+    ]
+);
