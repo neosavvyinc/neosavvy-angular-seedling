87f68a4186969f898a56b460a9b08975df210c4a
Adam Parrish <aparrish@neosavvy.com>
2013-12-11 14:42:50 -0500

    Updating some more build stuff to keep in sync with our recent developments.

--- karma.conf.js
+++ karma.conf.js
@@ -1,79 +1,85 @@
 // Karma configuration
 // Generated on Thu Sep 26 2013 16:43:03 GMT-0400 (EDT)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
-
-
-    // frameworks to use
-    frameworks: ['jasmine'],
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-        'src/main/resources/lib/lodash/lodash.compat.js',
-        'src/main/resources/lib/momentjs/moment.js',
-        'src/main/resources/lib/neosavvy-javascript-core/neosavvy-javascript-core.js',
-        'src/main/resources/lib/q/q.js',
-        'src/main/resources/lib/zepto/zepto.js',
-        'src/main/resources/lib/angular/angular.js',
-        'src/main/resources/lib/angular-*/*.js',
-        'src/main/resources/lib/neosavvy-javascript-angular-core/neosavvy-javascript-angular-core.js',
-        'src/main/resources/application/application.js',
-        'src/main/resources/core/**/*.js',
-        'src/main/resources/application/**/*.js',
-        'src/test/**/*-spec.js'
-    ],
+        // frameworks to use
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-        '**/Gruntfile.js'
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'src/main/resources/lib/node-uuid/uuid.js',
+            'src/main/resources/lib/lodash/lodash.compat.js',
+            'src/main/resources/lib/underscore.string/lib/underscore.string.js',
+            'src/main/resources/lib/momentjs/moment.js',
+            'src/main/resources/lib/neosavvy-javascript-core/neosavvy-javascript-core.js',
+            'src/main/resources/lib/q/q.js',
+            'src/main/resources/lib/angular/angular.js',
+            'src/main/resources/lib/angular-*/*.js',
+            'src/main/resources/lib/neosavvy-javascript-angular-core/neosavvy-javascript-angular-core.js',
+            'src/main/resources/application/application.js',
+            'src/main/resources/application/**/*.js',
+            'src/test/**/*-spec.js'
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+        // list of files to exclude
+        exclude: [
+        ],
+
+        preprocessors: {
+            'src/main/resources/application/**/*.js': 'coverage',
+        },
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress', 'coverage', 'junit'],
+
+        junitReporter: {
+            outputFile: 'target/coverage/cobertura.unit.xml'
+        },
+
+        // web server port
+        port: 9876,
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['Chrome'],
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Chrome'],
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
